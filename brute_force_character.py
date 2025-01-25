2376161be9d06df3e3c7cfe2c1226e1064ab0605
KwameCode27 <mickeyfent3@gmail.com>
2025-01-25 16:27:18 +0000

    file uploaded

--- brute_force_character.py
+++ brute_force_character.py
@@ -2,7 +2,7 @@ import random
 import pyautogui
 
 
-character= "0123456789abcdefghijklmnopqrstuvwxyz"
+character= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 character_list = list(character)
 
 password = input("Enter your password:")
@@ -11,6 +11,6 @@ while (guess != password):
     # checking random alphanumeric
     guess = random.choices(character_list, k=len(password))
     guess = "".join(guess)
-    # print(guess)
+    print(guess)
 
 print(f"Your password is : {guess}")
